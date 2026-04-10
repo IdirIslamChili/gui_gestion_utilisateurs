1db49c07a03d58cffbf32ca105d12bacce6e8e6d
Idir Islam Chili <chiliidirislam@gmail.com>
2026-04-09 23:32:18 -0400

    Modification de l'ajout des utilisateurs en utilisant le service créé et l'affichage des utilisateurs inscrit avec sucées récupérées de la base de données

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 from data.data_utilisateur import DataUtilisateur
 from models.utilisateur import Utilisateur
+from services.services_utilisateur import ServiceUtilisateur
 data_user=DataUtilisateur()
 con=data_user.connecter_db()
 print(con)
 
 user1=Utilisateur(10013, "ali", "sarah", 24)
-user2=Utilisateur(10014, "Anis", "Hsissou", 24)
-user3=Utilisateur(10015, "jiji", "kiki", 24)
-data_user.ajouter_utilisateur(user1)
-data_user.ajouter_utilisateur(user2)
-data_user.ajouter_utilisateur(user3)
+user2=Utilisateur(10014, "Anis", "Hsissou", 20)
+user3=Utilisateur(10015, "jiji", "kiki", 17)
+service = ServiceUtilisateur()
+print(service.inscrire_utilisateur(user1))
+print(service.inscrire_utilisateur(user2))
+print(service.inscrire_utilisateur(user3))
 
-utilisateurs=data_user.recuperer_utilisateur()
+utilisateurs = service.lister_utilisateurs()
+
+print("Liste des utilisateurs :")
+
 for u in utilisateurs:
-    print(u)
+    u.afficher_infos()
+
+
 
