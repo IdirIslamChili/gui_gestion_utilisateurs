cc3a3485a68e79bd02388628158f2431b8ab7c8a
Idir Islam Chili <chiliidirislam@gmail.com>
2026-04-17 13:37:11 -0400

    Modification de l'inscription des utilisateurs

--- main.py
+++ main.py
@@ -5,18 +5,17 @@ data_user=DataUtilisateur()
 con=data_user.connecter_db()
 print(con)
 
-user1=Utilisateur(10013, "ali", "sarah", 24)
-user2=Utilisateur(10014, "Anis", "Hsissou", 20)
-user3=Utilisateur(10015, "jiji", "kiki", 17)
+user1=Utilisateur(10013, "Idir", "Chili", 24)
+user2=Utilisateur(10014, "Anis", "Hacene", 20)
+user3=Utilisateur(10015, "Lyes", "Gulissa", 17)
 service = ServiceUtilisateur()
-print(service.inscrire_utilisateur(user1))
-print(service.inscrire_utilisateur(user2))
-print(service.inscrire_utilisateur(user3))
+service.inscrire_utilisateur(user1)
+service.inscrire_utilisateur(user2)
+service.inscrire_utilisateur(user3)
 
 utilisateurs = service.lister_utilisateurs()
 
 print("Liste des utilisateurs :")
-
 for u in utilisateurs:
     u.afficher_infos()
 
